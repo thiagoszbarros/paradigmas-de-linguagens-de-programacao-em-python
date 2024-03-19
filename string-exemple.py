438985ffd8f691c31f3374a97c578fcf616a0d46
Thiago Barros <thiagobarros95@gmail.com>
2024-03-18 21:14:52 -0300

    feat: add string exemple

---
+++ string-exemple.py
@@ -0,0 +1,6 @@
+nome = 'exemplo de string em python'
+
+print(nome)
+print('valor da variável nome = {}'.format(nome))
+print(f'valor da variável nome = {nome}')
+print(f"valor da variável nome = {nome}")
